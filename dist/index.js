c37f5c3fada0cbe091a78bed3b72feb149a2f2fd
Michael Sharp <msharp@amplience.com>
2020-06-26 14:55:49 +0100

    removed dist, added typescript as a dev dependency

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ImportedMessage_1 = require("./ImportedMessage");
-var msg = new ImportedMessage_1.ImportedMessage();
-(function () {
-    console.log("hello world!");
-})();
